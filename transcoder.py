4472108f8f54ff01ee4da332d9e2ba1a3bcb3eac
Victor Rojas <victorrojas@invisionapp.com>
2018-04-22 21:49:42 -0500

    check if download url is available in slack file

--- transcoder.py
+++ transcoder.py
@@ -123,7 +123,7 @@ class SlackInterfacer(object):
 
 			for f in files:
 				# We're only interested in demos not uploaded by the videobot itself
-				if 'url_private_download' not in video_url:
+				if 'url_private_download' not in f:
 					continue
 				video_url = f['url_private_download']
 				if f['user'] != self.BOT_ID and (video_url not in deleted_files):
